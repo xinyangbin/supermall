35dca60bc8f4775d8a43dc55253d4c604b7bdbd0
xinyangbin <2528089035@qq.com>
2020-09-13 12:10:55 +0800

    9.13

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,17 +6,37 @@
         购物街
       </div>
     </nav-bar>
+    <swiper>
+      <swiper-item v-for="(item, index) in banners" :key="index">
+        <a :href="item.link">
+          <img :src="item.image" alt="">
+        </a>
+      </swiper-item>
+    </swiper>
   </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar'
+  // import Swiper from 'components/common/swiper/Swiper'
+  // import SwiperItem from 'components/common/swiper/SwiperItem'
   import {getHomeMultidata} from 'network/home'
+  import {Swiper, SwiperItem} from 'components/common/swiper'
+
 
   export default {
     name:"Home",
     components: {
-      NavBar
+      NavBar,
+      Swiper,
+      SwiperItem
+    },
+    data() {
+      return {
+        // result: null
+        banners: [],
+        recommands: []
+      }
     },
     // 创建生命周期函数
     created() {
@@ -24,6 +44,11 @@
       getHomeMultidata()
       .then(res => {
         console.log(res);
+        // 此处this.result拿到的是组件对象的内容
+        // 将数据存储在result这个变量中
+        // this.result = res;
+        this.banners = res.data.banner.list;
+        this.recommands = res.data.recommand;
       })
       .catch(err => {
         console.log(err);
